d55bf404e352859489825a3da005a7777eabcc4c
Dave North <dave.north@rewind.io>
2019-10-25 18:17:53 -0400

    Format as JSON

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 sh -c "git diff --diff-filter=d --name-only origin/master"
-sh -c "git diff --diff-filter=d --name-only origin/master | xargs rubocop"
+sh -c "git diff --diff-filter=d --name-only origin/master | xargs rubocop --format json"
