f205dcf450bfe59dab0470ab6b3f15a80ad2ebbe
Dave North <dave.north@rewind.io>
2019-10-31 15:37:11 -0400

    Drop in new command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-set -x
-
-echo "Running Rubocop on these files...."
-git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$'
 
 echo "Running Rubocop..."
-git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json
+#git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json
+git diff-tree -r --no-commit-id --name-only master@\{u\} head | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop
