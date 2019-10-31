0f9af3052d58d1c8a24a6059d4b5b1ea74e118e3
Dave North <dave.north@rewind.io>
2019-10-31 14:59:09 -0400

    Only run on .rb files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-sh -c "git diff --diff-filter=d --name-only origin/master"
-sh -c "git diff --diff-filter=d --name-only origin/master | xargs rubocop --format json"
+echo "Running Rubocop on these files...."
+sh -c "git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$''"
+
+echo "Running Rubocop..."
+sh -c "git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json"
