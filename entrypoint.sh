93a7b54d54f6cdb09a1556dc4e54ef2bcd723eea
Dave North <dave.north@rewind.io>
2019-10-31 15:18:15 -0400

    No need for subshell

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Running Rubocop on these files...."
-sh -c "git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$'"
+git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$'
 
 echo "Running Rubocop..."
-sh -c "git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json"
+git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json
