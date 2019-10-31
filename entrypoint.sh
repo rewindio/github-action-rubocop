89d6be1a4b57142af316015fd6905eb312bed910
Dave North <dave.north@rewind.io>
2019-10-31 15:59:42 -0400

    No fetch

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 
 echo "Running Rubocop..."
 #git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json
-git fetch
 git diff-tree -r --no-commit-id --name-only master@\{u\} HEAD | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop
