ea5a96251b9cc41d93ba77e47ea99e85212c60d4
Dave North <dave.north@rewind.io>
2019-10-31 16:15:47 -0400

    Back to how we were filtering

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 echo "Running Rubocop..."
-#git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json
-git diff-tree -r --no-commit-id --name-only master@\{u\} HEAD | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop
+git diff --diff-filter=d --name-only origin/master |grep '\.rb$' |xargs rubocop
