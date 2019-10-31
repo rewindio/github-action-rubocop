8bff9ac3045c23389ac975fa02c8db71a3f26e87
Dave North <dave.north@rewind.io>
2019-10-31 15:10:40 -0400

    Quoting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Running Rubocop on these files...."
-sh -c "git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$''"
+sh -c "git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$'"
 
 echo "Running Rubocop..."
 sh -c "git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json"
