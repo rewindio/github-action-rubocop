d56e8da0096ccc7921dd6b48154df512f94c8dee
Dave North <dave.north@rewind.io>
2019-10-31 15:51:41 -0400

    Get all branches so we can compare to master

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 echo "Running Rubocop..."
 #git diff --diff-filter=d --name-only origin/master | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop --format json
-git diff-tree -r --no-commit-id --name-only master@\{u\} head | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop
+git fetch
+git diff-tree -r --no-commit-id --name-only master@\{u\} HEAD | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop
