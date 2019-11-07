5dedf7e15ab3356ebd100563f2f67fae5c19e84d
Dave North <dave.north@rewind.io>
2019-11-07 07:58:03 -0500

    Add problem manager to annotate

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo "Running Rubocop..."
 
+echo "::add-matcher::rubocop-problem-matcher.json"   
+
 file_list=$(git diff --diff-filter=d --name-only origin/master |grep '\.rb$')
 
 if [ -z "$file_list" ]; then
