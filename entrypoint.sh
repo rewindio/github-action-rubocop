fabed098ae8ee3bc77dc0491cbf5bbf8837b123d
Dave North <dave.north@rewind.io>
2019-11-07 10:49:25 -0500

    Add some comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,13 @@
 
 echo "Running Rubocop..."
 
+# With problem matchers in a container, the matcher config MUST be available outside the container on the VM
+# So we will just copy it into the workspace
 matcher_path=`pwd`/rubocop-problem-matcher.json
-
 cp /rubocop-problem-matcher.json "$matcher_path"
 
+# Note here that we do not use the matcher-path since this is a bind mount into the container
+# and is not the same path outside the container on the VM.  Instead, just use current dir
 echo "::add-matcher::rubocop-problem-matcher.json"   
 
 file_list=$(git diff --diff-filter=d --name-only origin/master |grep '\.rb$')
